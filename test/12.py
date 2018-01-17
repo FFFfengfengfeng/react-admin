ea368afaca538edb65068eb9f4e26f12c0e704e5
FFFfengfengfeng <1098865109@qq.com>
2018-01-17 18:06:41 +0800

    15

--- test/12.py
+++ test/12.py
@@ -2,4 +2,29 @@
 
 # 题目：判断101-200之间有多少个素数，并输出所有素数。
 
+# 定义函数判断一个数字是否是素数
+import math
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+
+
+print isPrime(125)
+
+result = []
+# 循环判断101-200中的素数
+for i in range(101, 200):
+    flag = isPrime(i)
+    if flag:
+        result.append(i)
+    else:
+        continue
+
+print result
+print '101-200之间的素数有: ', len(result), '个'
 
