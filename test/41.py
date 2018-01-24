6a3e24628b112cffa0961b7855156e27fd6f6dee
FFFfengfengfeng <1098865109@qq.com>
2018-01-24 10:31:47 +0800

    静态变量

--- test/41.py
+++ test/41.py
@@ -1,3 +1,15 @@
 # -*- coding: UTF-8 -*-
 
 # 题目：模仿静态变量的用法。
+
+class Static:
+    StaticVar = 5
+    def varfun(self):
+        self.StaticVar += 1
+        print self.StaticVar
+
+print Static.StaticVar
+a = Static()
+
+for i in range(3):
+    a.varfun()
