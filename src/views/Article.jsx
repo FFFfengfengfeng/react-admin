b6fb1ab3eb4d4777776834cb6def59cc388e7a1f
FFFfengfengfeng <1098865109@qq.com>
2018-04-26 11:23:52 +0800

    table样式

--- src/views/Article.jsx
+++ src/views/Article.jsx
@@ -16,7 +16,55 @@ class Article extends Component {
     render() {
         return (
             <div className='main'>
-                <p>文章管理</p>
+                <div className='warpper'>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>
+                                    <input type="checkbox"/>
+                                </th>
+                                <th>id</th>
+                                <th>标题</th>
+                                <th>浏览</th>
+                                <th>评论</th>
+                                <th>日期</th>
+                                <th>操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <input type="checkbox"/>
+                                </td>
+                                <td>
+                                    <p>1</p>
+                                </td>
+                                <td>
+                                    <p>1</p>
+                                </td>
+                                <td>
+                                    <p>1</p>
+                                </td>
+                                <td>
+                                    <p>1</p>
+                                </td>
+                                <td>
+                                    <p>1</p>
+                                </td>
+                                <td>
+                                    <p>1</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className='page'>
+                        <a href="" className='active'>1</a>
+                        <a href="">2</a>
+                        <a href="">3</a>
+                        <a href="">4</a>
+                        <a href="">6</a>
+                    </div>
+                </div>
             </div>
         );
     }
